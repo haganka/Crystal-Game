87f1d0890db525976c8e98c05f82d68779c3f48a
Kelly <kelly.hagan3@gmail.com>
2018-01-25 17:20:11 -0600

    adds page structure in HTML and sets variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    
+    var randNum = 0;
+    var greenNum = 0;
+    var redNum = 0;
+    var blueNum = 0;
+    var purpleNum = 0;
+    var total = 0;
+    var matchRandom = false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});

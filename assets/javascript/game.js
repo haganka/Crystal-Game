53e6fa24d6248c0a3749ac6de71f72d0945a8d52
Kelly <kelly.hagan3@gmail.com>
2018-01-26 14:55:13 -0600

    fixed css link and crystal size

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,32 @@
 $(document).ready(function () {
     
-    var randNum = 0;
-    var greenNum = 0;
-    var redNum = 0;
-    var blueNum = 0;
-    var purpleNum = 0;
-    var total = 0;
+    var greenNum;
+    var redNum;
+    var blueNum;
+    var purpleNum;
+    var total;
     var matchRandom = false;
 
+    var randNum = Math.floor((Math.random() * 100) + 1);
+    console.log(randNum);
+    $(".randomBox").text("Random Number: " + randNum);
 
+    var randCrystalNum = function (){
+        
+    }
 
+    $("#blueCrystal").on("click", randCrystalNum);
+    $("#greenCrystal").on("click", randCrystalNum);
+    $("#redCrystal").on("click", randCrystalNum);
+    $("#purpleCrystal").on("click", randCrystalNum);
 
+    //not working
+    // var startGame = function(){
 
+    
+    // }
 
-
-
-
-
-
-
-
-
+// startGame();
 
 
 
